1813f9640ecb1529bfd90eecda8cb78b02d0e2ae
Joshua Dada <joshwebprog@gmail.com>
2025-09-20 21:31:55 +0100

    General Operations

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -46,7 +46,8 @@ export default function RootLayout() {
           {/* <RouteGuard> */}
           <Stack screenOptions={{headerShown: false}}>
             {/* <Stack.Screen  name={!isAuth ? "login" : "(tabs)" } options={{headerShown: false}} /> */}
-            <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+            {/* <Stack.Screen name="(tabs)" options={{headerShown: false}} /> */}
+            <Stack.Screen name="(screen)" options={{headerShown: false}} />
           </Stack>
           {/* </RouteGuard> */}
         </SafeAreaProvider>
