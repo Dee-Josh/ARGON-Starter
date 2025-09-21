e00efc3c13e081ce7e191605a688157d132167fe
Joshua Dada <joshwebprog@gmail.com>
2025-09-21 17:14:32 +0100

    General Operations

--- app/onboarding/_layout.tsx
+++ app/onboarding/_layout.tsx
@@ -1,15 +1,13 @@
-import { AuthProvider, useAuth } from "@/lib/context/AuthContext";
+
 import { Stack, useRouter, useSegments } from "expo-router";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 
 export default function AuthLayout() {
   return(
     <Stack screenOptions={{headerShown: false}}>
-      {/* <Stack.Screen  name={!isAuth ? "login" : "(tabs)" } options={{headerShown: false}} /> */}
-      {/* <Stack.Screen name="(tabs)" options={{headerShown: false}} /> */}
-      <Stack.Screen name="streaks" options={{headerShown: false}} />
+      <Stack.Screen name="SignUp" options={{headerShown: false}} />
+      <Stack.Screen name="LogIn" options={{headerShown: false}} />
+      <Stack.Screen name="ForgotPassword" options={{headerShown: false}} />
     </Stack>
   )
 }
