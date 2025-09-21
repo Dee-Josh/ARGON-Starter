d58c56ca76adebade59d57ef9062583474a22f21
Joshua Dada <joshwebprog@gmail.com>
2025-09-21 17:06:54 +0100

    General Operations

--- app/onboarding/_layout.tsx
+++ app/onboarding/_layout.tsx
@@ -1,23 +1,15 @@
 import { AuthProvider, useAuth } from "@/lib/context/AuthContext";
 import { Stack, useRouter, useSegments } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 
-export default function RootLayout() {
+export default function AuthLayout() {
   return(
-    <GestureHandlerRootView style={{flex: 1}}>
-      <SafeAreaProvider>
-        {/* <RouteGuard> */}
-        <Stack screenOptions={{headerShown: false}}>
-          {/* <Stack.Screen  name={!isAuth ? "login" : "(tabs)" } options={{headerShown: false}} /> */}
-          {/* <Stack.Screen name="(tabs)" options={{headerShown: false}} /> */}
-          <Stack.Screen name="streaks" options={{headerShown: false}} />
-        </Stack>
-        {/* </RouteGuard> */}
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
+    <Stack screenOptions={{headerShown: false}}>
+      {/* <Stack.Screen  name={!isAuth ? "login" : "(tabs)" } options={{headerShown: false}} /> */}
+      {/* <Stack.Screen name="(tabs)" options={{headerShown: false}} /> */}
+      <Stack.Screen name="streaks" options={{headerShown: false}} />
+    </Stack>
   )
 }
