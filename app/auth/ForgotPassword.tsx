27b92e054701b7c80443368a751bb586073c6102
Joshua Dada <joshwebprog@gmail.com>
2025-09-27 21:05:12 +0100

    Working on Forgot Password

--- app/auth/ForgotPassword.tsx
+++ app/auth/ForgotPassword.tsx
@@ -1,13 +1,26 @@
-import { View, Text, Button, ImageBackground } from "react-native";
+import { View, Text, Button, StyleSheet, ImageBackground } from "react-native";
 import { useRouter } from "expo-router";
 
 export default function ForgotPassword() {
   const router = useRouter();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={styles.container}>
       <Text style={{ fontSize: 24 }}>FORGOT PASSWORD</Text>
       <Button title="Next" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  imgBg: {
+
+  },
+  container: {
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center",
+    
+  },
+
+})
